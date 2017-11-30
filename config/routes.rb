a8b8dab7ed1b70b43eea8df33abef3e5c691f58d
Pierre-Alexis Bayen <bayen.pierre@gmail.com>
2017-11-30 13:54:51 +0100

    end of the cocktail controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  # get 'cocktails/index'
+
+  # get 'cocktails/new'
+
+  # get 'cocktails/create'
+
+  # get 'cocktails/show'
+
+  # get 'cocktails/edit'
+
+  # get 'cocktails/update'
+
+  # get 'cocktails/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'cocktails#index'
+resources :cocktails do
+  resources :doses, only: [ :new, :create, :destroy ]
+end
+
 end
